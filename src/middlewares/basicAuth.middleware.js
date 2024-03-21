3fcfcbc47c883ea204a6b0629ada2a4a6a8f33a4
yash1370 <patelyash1018@gmail.com>
2024-03-21 16:08:06 +0530

    authorization done

--- src/middlewares/basicAuth.middleware.js
+++ src/middlewares/basicAuth.middleware.js
@@ -1,10 +1,8 @@
 import UserModel from "../features/user/user.model.js";
 
 const basicAuthorization = (req, res, next) => {
-  const authHeader = req.headers["auth"];
-
   //   1. authorization header is empty
-
+  const authHeader = req.headers["authorization"];
   if (!authHeader) {
     return res.status(401).send("No Authorization access");
   }
@@ -15,7 +13,7 @@ const basicAuthorization = (req, res, next) => {
   console.log("base64Credentials: ", base64Credentials);
 
   //    3. Decode credentials
-  const decodedCredentials = Buffer.form(base64Credentials, "base64").toString(
+  const decodedCredentials = Buffer.from(base64Credentials, "base64").toString(
     "utf8"
   );
   console.log("decodedCredentials: ", decodedCredentials);
