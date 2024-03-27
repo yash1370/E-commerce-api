a3d6d445a41af0553c3c65931094e0b903edf8aa
yash1370 <patelyash1018@gmail.com>
2024-03-27 13:01:53 +0530

    swagger

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // 1. Import express
 import express from "express";
+import swagger from "swagger-ui-express";
+
 // import bodyParser from 'body-parser';
 import productRouter from "./src/features/product/product.routes.js";
 import userRouter from "./src/features/user/user.routes.js";
 import jwtAuth from "./src/middlewares/jwt.middleware.js";
 import cookieParser from "cookie-parser";
 import cartItemsRouter from "./src/features/cartItems/cartItems.routes.js";
+import apiDocs from "./swagger.json" assert { type: "json" };
 
 // 2. Create Server
 const server = express();
@@ -14,6 +17,7 @@ server.use(express.json());
 server.use(cookieParser());
 // for all requests related to product, redirect to product routes.
 // localhost:3200/api/products
+server.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
 server.use("/api/products", jwtAuth, productRouter);
 server.use("/api/users", userRouter);
 server.use("/api/cartItems", jwtAuth, cartItemsRouter);
